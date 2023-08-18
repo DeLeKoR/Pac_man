7229f8775b591a73d1a756307f83b89cba46f715
dmitry <dumarzhev@gmail.com>
2023-08-18 19:13:18 +1000

    оптимизировал перемещение, добавлено управление стрелками

--- Core.py
+++ Core.py
@@ -16,13 +16,13 @@ while True:
             pg.quit()
             sys.exit()
         if event.type == pg.KEYDOWN:
-            if event.key == pg.K_w:
+            if event.key == pg.K_w or event.key == pg.K_UP:
                 game.pac_man.move_future = (0, -SPEED)
-            elif event.key == pg.K_s:
+            elif event.key == pg.K_s or event.key == pg.K_DOWN:
                 game.pac_man.move_future = (0, SPEED)
-            elif event.key == pg.K_a:
+            elif event.key == pg.K_a or event.key == pg.K_LEFT:
                 game.pac_man.move_future = (-SPEED, 0)
-            elif event.key == pg.K_d:
+            elif event.key == pg.K_d or event.key == pg.K_RIGHT:
                 game.pac_man.move_future = (SPEED, 0)
 
 
--- Entity.py
+++ Entity.py
@@ -10,57 +10,52 @@ class Entity:
         self.move_future = (0, 0)
         # клетка на которой расположен объект
         self.cell = cell
+        self.future_cell = None
         # все клетки
         self.cells = cells
         # координаты (по умолчанию совмещают центр объекта с центром клетки)
-        self.x = cell.real_cord[0]-(self.size[0]/2-cell.cell_size[0]/2)-1
-        self.y = cell.real_cord[1]-(self.size[1]/2-cell.cell_size[1]/2)
-        self.rect = pg.Rect(self.x, self.y, *self.size)
+        self.rect = pg.Rect(0, 0, *self.size)
+        self.rect.center = self.cell.rect.center
+        self.x = self.rect.x
+        self.y = self.rect.y
 
     def move(self):
-        if self.move_now[0] > 0:
-            cell = get_cell_by_cord((self.cell.cord[0]+1, self.cell.cord[1]), self.cells)
-            if cell.type:
+        try:
+            if self.rect.center == get_cell(self.rect.center, self.cells).rect.center:
+                self.update_cell(get_cell(self.rect.center, self.cells))
+                if self.move_future[1] > 0:
+                    cell = get_cell_by_cord((self.cell.cord[0], self.cell.cord[1]+1), self.cells)
+                    if self.move_now == self.move_future:
+                        self.future_cell = cell
+                    if cell.type:
+                        self.move_now = self.move_future
+                elif self.move_future[1] < 0:
+                    cell = get_cell_by_cord((self.cell.cord[0], self.cell.cord[1]-1), self.cells)
+                    if self.move_now == self.move_future:
+                        self.future_cell = cell
+                    if cell.type:
+                        self.move_now = self.move_future
+                elif self.move_future[0] > 0:
+                    cell = get_cell_by_cord((self.cell.cord[0]+1, self.cell.cord[1]), self.cells)
+                    if self.move_now == self.move_future:
+                        self.future_cell = cell
+                    if cell.type:
+                        self.move_now = self.move_future
+                elif self.move_future[0] < 0:
+                    cell = get_cell_by_cord((self.cell.cord[0]-1, self.cell.cord[1]), self.cells)
+                    if self.move_now == self.move_future:
+                        self.future_cell = cell
+                    if cell.type:
+                        self.move_now = self.move_future
+            if self.future_cell is not None and self.future_cell.type:
                 self.x += self.move_now[0]
-        elif self.move_now[0] < 0:
-            cell = get_cell_by_cord((self.cell.cord[0]-1, self.cell.cord[1]), self.cells)
-            if cell.type:
-                self.x += self.move_now[0]
-        elif self.move_now[1] > 0:
-            cell = get_cell_by_cord((self.cell.cord[0], self.cell.cord[1]+1), self.cells)
-            if cell.type:
                 self.y += self.move_now[1]
-        elif self.move_now[1] < 0:
-            cell = get_cell_by_cord((self.cell.cord[0], self.cell.cord[1]-1), self.cells)
-            if cell.type:
-                self.y += self.move_now[1]
-        self.rect.x, self.rect.y = self.x, self.y
-
-        if self.rect.center == get_cell(self.rect.center, self.cells).rect.center:
-            self.update_cell(get_cell(self.rect.center, self.cells))
-            if any([(self.move_now[0] and self.move_future[0]),
-                    (self.move_now[1] and self.move_future[1])]):
-                self.move_now = self.move_future
-            elif self.move_future[1] > 0:
-                cell = get_cell_by_cord((self.cell.cord[0], self.cell.cord[1]+1), self.cells)
-                if cell.type:
-                    self.move_now = self.move_future
-            elif self.move_future[1] < 0:
-                cell = get_cell_by_cord((self.cell.cord[0], self.cell.cord[1]-1), self.cells)
-                if cell.type:
-                    self.move_now = self.move_future
-            elif self.move_future[0] > 0:
-                cell = get_cell_by_cord((self.cell.cord[0]+1, self.cell.cord[1]), self.cells)
-                if cell.type:
-                    self.move_now = self.move_future
-            elif self.move_future[0] < 0:
-                cell = get_cell_by_cord((self.cell.cord[0]-1, self.cell.cord[1]), self.cells)
-                if cell.type:
-                    self.move_now = self.move_future
+                self.rect.x, self.rect.y = self.x, self.y
+        except AttributeError:
+            pass
 
     def update_cell(self, cell):
         self.cell = cell
-        if self.move_now[0]:
-            self.y = cell.real_cord[1] - (self.size[1] / 2 - cell.cell_size[1] / 2)
-        if self.move_now[1]:
-            self.x = cell.real_cord[0] - (self.size[0] / 2 - cell.cell_size[0] / 2)-1
+        self.rect.center = self.cell.rect.center
+        self.x = self.rect.x
+        self.y = self.rect.y
