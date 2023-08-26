424f70cba8eebf66dc80c20d62e16d24bffec5d7
IliaSV06 <shikovi78@gmail.com>
2023-08-26 22:56:40 +1000

    Сделал перемещение по лабиринту для призраков

--- Game_core.py
+++ Game_core.py
@@ -3,22 +3,26 @@ from Map import *
 from Enemies import *
 from Pac_man import *
 from Basic_func import *
+from Score_board import *
 
 class Game:
     def __init__(self, screen):
         self.screen = screen
         self.map = Map(self.screen)
-        self.pac_man = Pac_man(get_cell_by_cord((13, 23), self.map.cells), self.screen, self.map.cells)
+        self.pac_man = Pac_man(get_cell_by_cord((2, 14), self.map.cells), self.screen, self.map.cells)
         self.enemies = pg.sprite.Group()
         self.create_enemies()
+        self.score_board = Score_board(self.screen)
 
     def draw_frame(self):
-        self.map.draw()
-        #self.map.draw_cells()
-        self.pac_man.draw()
+        self.map.draw_map()
+        self.map.draw_points()
+        self.pac_man.draw_pac_man()
         self.enemies.draw(self.screen)
+        self.score_board.draw_board()
 
     def create_frame(self):
+        self.pac_man.eat_point(self.score_board.score)
         self.pac_man.move()
         self.enemies.update()
 
